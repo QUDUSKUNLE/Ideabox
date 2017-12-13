27183c401cdd8583fca3c13569d8178e9c73c6dc
abdulquddus <qudus.yekeen@andela.com>
2017-12-13 10:09:20 +0100

    [Chore] - Move createIdea into idea folder

---
+++ client/src/components/container/idea/Idea.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+/**
+ * @function Idea
+ * @description the Idea component.
+ * @returns {string} The HTML markup for the Idea component
+ */
+export default class Idea extends React.Component {
+  /**
+   * Create a constructor
+   * @constructor
+   * @param {object} props
+   * @memberof Idea
+   */
+  constructor(props) {
+    super(props);
+    this.state = { publicIdea: [] };
+  }
+
+  /**
+  * @method componentWillReceiveProps
+  * @description This listening to event in the AppStore
+  * @param {object} nextProps
+  * @return {void}
+  * @memberof MainIdea
+  */
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.publicIdea[0] !== undefined) {
+      this.setState({
+        publicIdea: nextProps.publicIdea
+      });
+    }
+  }
+
+  /**
+  * @description - render method, React lifecycle method
+  * @returns {object} CommentPage component
+  */
+  render() {
+    if (this.state.publicIdea[0] !== undefined) {
+      return (
+        <div className="idea">
+          <div className="row">
+            {this.state.publicIdea.map(idea =>
+              (
+                <div className="col s12 m6" key={idea._id}>
+                  <div className="card">
+                    <div className="card-content">
+                      <span
+                        className="card-title activator grey-text text-darken-4"
+                      >{idea.title}
+                        <i className="material-icons right">more_vert
+                        </i>
+                      </span>
+                      <p>
+                        <a href="#">
+                          {`${idea.description.slice(0, 20)} ....`}
+                        </a>
+                      </p>
+                    </div>
+                    <div className="card-reveal black-text">
+                      <span
+                        className="card-title black-text"
+                      >{idea.title}
+                        <i className="material-icons right">close
+                        </i>
+                      </span>
+                      <p>{idea.description}.</p>
+                      <p>Author:{' '}
+                        <span className="deep-purple-text">
+                          {idea.author.name}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="card-action">
+                      <span className="black-text">{idea.category[0]}</span>
+                      <Link to={`/dashboard/${idea._id}`}>
+                        <i
+                          href=""
+                          role="button"
+                          tabIndex="0"
+                          className="material-icons black-text delete_tag edit"
+                        >more
+                        </i>
+                      </Link>
+                      <i
+                        role="button"
+                        tabIndex="0"
+                        className="material-icons black-text removable"
+                      >
+                        share
+                      </i>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      );
+    }
+    return true;
+  }
+}
+
+Idea.propTypes = {
+  publicIdea: PropTypes.array.isRequired
+};
+
---
+++ client/src/components/container/idea/MainIdea.jsx
@@ -0,0 +1,164 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppActions from '../../../actions/AppActions';
+import AppConstants from '../../../contants/AppConstants';
+import AppStore from '../../../store/AppStore';
+import Comment from '../../container/comment/Comment';
+
+/**
+ * @description - renders MainIdea Component
+ * @class MainIdea
+ * @extends {React.Component}
+ */
+export default class MainIdea extends React.Component {
+  /**
+   * Create a constructor
+   * @constructor
+   * @param {object} props
+   * @memberof MainIdea
+   */
+  constructor(props) {
+    super(props);
+    this.state = { comment: '', fetchComment: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleComment = this.handleComment.bind(this);
+  }
+
+  /**
+  * @method componentDidMount
+  * @description This listening to event in the AppStore
+  * @return {void}
+  * @memberof MainIdea
+  */
+  componentDidMount() {
+    AppStore.on(AppConstants.CREATE_COMMENT, this.commentResponse);
+  }
+
+  /**
+  * @method componentWillReceiveProps
+  * @description This listening to event in the AppStore
+  * @param {object} nextProps
+  * @return {void}
+  * @memberof MainIdea
+  */
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.fetchComment[0] !== undefined) {
+      this.setState({
+        fetchComment: nextProps.fetchComment
+      });
+    }
+  }
+
+  /**
+  * @method componentWillUnmount
+  * @description Removes listener from AppStore
+  * @return {void}
+  * @memberof MainIdea
+  */
+  componentWillUnmount() {
+    AppStore.removeListener(AppConstants.CREATE_COMMENT, this.commentResponse);
+  }
+
+  /**
+  * @method handleChange
+  * @description class method that binds comment input to form field
+  * @return {void}
+  * @param {event} event
+  */
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  /**
+  * @method handleComment
+  * @description class method that sends comments
+  * @return {void}
+  * @param {event} event
+  */
+  handleComment(event) {
+    event.preventDefault();
+    const commentDetails = {
+      ideaId: this.props.idea._id,
+      comment: this.state.comment
+    };
+    AppActions.sendComment(commentDetails);
+    this.setState({ comment: '' });
+  }
+
+  /**
+  * @method commentResponse
+  * @description class method that comments response
+  * @return {void}
+  */
+  commentResponse() {
+    Materialize.toast(AppStore.createComment().message, 2000, 'rounded green');
+    AppActions.fetchComment(AppStore.createComment().createdComment.idea.id);
+  }
+
+
+  /**
+  * @description - render method, React lifecycle method
+  * @returns {Object} MainIdea component
+  */
+  render() {
+    return (
+      <div className="container">
+        <ul className="collapsible popout" data-collapsible="accordion">
+          <li>
+            <div className="collapsible-header black-text">
+              <i className="material-icons">
+                subtitles
+              </i>{this.props.idea.title}
+            </div>
+            <div className="collapsible-body white-text comment_header">
+              <span>{this.props.idea.description}</span>
+            </div>
+          </li>
+          <li>
+            <div className="collapsible-header active black-text">
+              <i className="material-icons">
+                comment
+              </i>
+            </div>
+            <div className="collapsible-body black-text comment">
+              <div className="messageBody">
+                <div className="row">
+                  <Comment
+                    fetchComment={this.state.fetchComment}
+                  />
+                </div>
+              </div>
+              <form
+                className="col s12"
+                onSubmit={this.handleComment}
+              >
+                <div className="row">
+                  <div className="input-field commentForm">
+                    <input
+                      name="comment"
+                      value={this.state.comment}
+                      onChange={this.handleChange}
+                      id="comment"
+                      type="text"
+                      required
+                    />
+                    <label
+                      htmlFor="comment"
+                    >Comment
+                    </label>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+MainIdea.propTypes = {
+  idea: PropTypes.object.isRequired,
+  fetchComment: PropTypes.array.isRequired
+};
+
