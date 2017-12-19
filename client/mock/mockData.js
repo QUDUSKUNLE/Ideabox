cffe1c21ce94db309d29406d7abda63a4d0ae868
abdulquddus <qudus.yekeen@andela.com>
2017-12-19 09:44:02 +0100

    [Chore #153739458] - Write AppActions test

---
+++ client/mock/mockData.js
@@ -0,0 +1,27 @@
+export default {
+  signUpUser: {
+    email: 'quduskunle@gmail.com',
+    password: 'Ka123@',
+    username: 'kunle'
+  },
+  logInUser: {
+    email: 'kunle@gmail.com',
+    password: 'kuieujksskksnsknsn'
+  },
+  resetPassword: {
+    email: 'kunle@gmail.com'
+  },
+  createIdea: {
+    title: 'The beginning of the world',
+    description: 'Nice Idea to implement',
+    access: 'Public',
+    category: 'Science & Technology'
+  },
+  ideaId: 'jadkasdksljcskadljckadslj',
+  ideaDetails: {
+    title: 'The beginning of the world',
+    description: 'Nice Idea to implement',
+    access: 'Public',
+    category: 'Science & Technology'
+  }
+};
